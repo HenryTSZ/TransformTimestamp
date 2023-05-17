e8b81a56fb34de09a89674208929c3b829110424
李亨(10023318) <lih-y@glodon.com>
2023-05-17 17:46:50 +0800

    init

--- build/index.js
+++ build/index.js
@@ -1,42 +1,43 @@
-import { Flow } from "flow-launcher-helper";
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime.js";
-import localizedFormat from "dayjs/plugin/localizedFormat.js";
-import childProcess from "child_process";
+import { Flow } from 'flow-launcher-helper';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime.js';
+import localizedFormat from 'dayjs/plugin/localizedFormat.js';
+import childProcess from 'child_process';
 dayjs.extend(localizedFormat);
 dayjs.extend(relativeTime);
-const copy = (content) => childProcess.spawn("clip").stdin.end(content);
-const { on, showResult, run } = new Flow("app.png");
-on("query", (params) => {
+const copy = (content) => childProcess.spawn('clip').stdin.end(content);
+const { on, showResult, run } = new Flow('app.png');
+on('query', params => {
+    params = params.filter(Boolean);
     let date = dayjs();
     if (params.length > 0) {
-        date = dayjs(params[0]);
+        date = dayjs(+params[0]);
     }
     if (date.isValid()) {
         showResult(...getResults(date));
         return;
     }
     showResult({
-        title: "Invalid input",
+        title: 'Invalid input'
     });
 });
 function getResults(date) {
+    const format = date.format('YYYY-MM-DD HH:mm:ss');
+    const value = `${date.valueOf()}`;
     return [
         {
-            title: date.format("L"),
-            subtitle: `${date.valueOf()}`,
-            method: "copy_result",
-            params: [`${date.valueOf()}`],
+            title: format,
+            method: 'copy_result',
+            params: [format]
         },
         {
-            title: `${date.valueOf()}`,
-            subtitle: date.format("L"),
-            method: "copy_result",
-            params: [date.format("L")],
-        },
+            title: value,
+            method: 'copy_result',
+            params: [value]
+        }
     ];
 }
-on("copy_result", (params) => {
+on('copy_result', params => {
     copy(params[0]);
 });
 run();
--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,56 @@
-import { Flow, JSONRPCResponse } from "flow-launcher-helper";
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime.js";
-import localizedFormat from "dayjs/plugin/localizedFormat.js";
-import childProcess from "child_process";
+import { Flow, JSONRPCResponse } from 'flow-launcher-helper'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime.js'
+import localizedFormat from 'dayjs/plugin/localizedFormat.js'
+import childProcess from 'child_process'
 
-dayjs.extend(localizedFormat);
-dayjs.extend(relativeTime);
+dayjs.extend(localizedFormat)
+dayjs.extend(relativeTime)
 
-const copy = (content: string) => childProcess.spawn("clip").stdin.end(content);
+const copy = (content: string) => childProcess.spawn('clip').stdin.end(content)
 
-type Methods = "copy_result";
+type Methods = 'copy_result'
 
-const { on, showResult, run } = new Flow("app.png");
+const { on, showResult, run } = new Flow('app.png')
 
-on("query", (params) => {
-	let date: dayjs.Dayjs = dayjs();
+on('query', params => {
+  params = params.filter(Boolean)
 
-	if (params.length > 0) {
-		date = dayjs(params[0] as number);
-	}
+  let date: dayjs.Dayjs = dayjs()
 
-	if (date.isValid()) {
-		showResult(...getResults(date));
-		return;
-	}
+  if (params.length > 0) {
+    date = dayjs(+params[0])
+  }
 
-	showResult({
-		title: "Invalid input",
-	});
-});
+  if (date.isValid()) {
+    showResult(...getResults(date))
+    return
+  }
+
+  showResult({
+    title: 'Invalid input'
+  })
+})
 
 function getResults(date: dayjs.Dayjs): JSONRPCResponse<Methods>[] {
-	return [
-		{
-			title: date.format("L"),
-			subtitle: `${date.valueOf()}`,
-			method: "copy_result",
-			params: [`${date.valueOf()}`],
-		},
-		{
-			title: `${date.valueOf()}`,
-			subtitle: date.format("L"),
-			method: "copy_result",
-			params: [date.format("L")],
-		},
-	];
+  const format = date.format('YYYY-MM-DD HH:mm:ss')
+  const value = `${date.valueOf()}`
+  return [
+    {
+      title: format,
+      method: 'copy_result',
+      params: [format]
+    },
+    {
+      title: value,
+      method: 'copy_result',
+      params: [value]
+    }
+  ]
 }
 
-on("copy_result", (params) => {
-	copy(params[0] as string);
-});
+on('copy_result', params => {
+  copy(params[0] as string)
+})
 
-run();
+run()
